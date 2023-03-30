cc2f27d3deb52cc42dc751f8ac49a46297fa0d97
Administrator <admin@example.com>
2023-03-30 11:51:26 +0800

    first commit

---
+++ VideoPlayer3.py
@@ -0,0 +1,252 @@
+import sys
+import os
+sys.path.append('I:/mmdeploy/build/bin/Release')
+os.add_dll_directory('D:/onnxruntime-win-x64-1.8.1/lib')
+os.add_dll_directory('D:/opencv/build/x64/vc16/bin')
+
+
+import cv2
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
+from mmdeploy_python import Detector, Segmentor, PoseDetector, VideoRecognizer
+from main import Ui_MainWindow
+import numpy as np
+
+class VideoPlayer(QObject):
+
+    finished = Signal()
+
+    def __init__(self, video_path):
+        QObject.__init__(self)
+        # 创建 VideoCapture 对象并指定要读取的视频文件路径
+        self.cap = cv2.VideoCapture(video_path)
+
+        # 获取视频的帧率、宽度和高度
+        self.fps = int(self.cap.get(cv2.CAP_PROP_FPS))
+        self.width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    # def set_video_path(self, video_path):
+    #     self.cap = cv2.VideoCapture(video_path)
+
+    def set_detector(self, detector):
+        self.detector = detector
+
+    def playVideo(self):
+        while True:
+            ret, frame = self.cap.read()
+
+            if not ret:
+              break
+            print(ret)
+            self.detector.detect(frame)
+        self.finished.emit()
+
+
+class YoloDetect(QObject):
+    
+    image = Signal(np.ndarray)
+    result = Signal(np.ndarray)
+
+    def __init__(self, model):
+        QObject.__init__(self)
+        self.detector = Detector('model/'+model, 'cpu', 0)
+
+
+    def detect(self, frame):
+    
+        frame_org = frame
+        frame = cv2.resize(frame, (int(852), int(480)))
+        bboxes, labels, _ = self.detector(frame)
+
+        # 使用阈值过滤推理结果，并绘制到原图中
+        indices = [i for i in range(len(bboxes))]
+        for index, bbox, label_id in zip(indices, bboxes, labels):
+            score = bbox[4]
+            if score < 0.7:
+                continue
+            # 绘制bounding box 和 label 文本    
+            self.draw_labels(frame, bbox, label_id)
+
+        self.image.emit(frame_org)
+        self.result.emit(frame)
+
+    def draw_labels(self, frame, bbox, label_id):
+        [left, top, right, bottom] = bbox[0:4].astype(int)
+
+        # 绘制矩形框
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+        # 绘制标签
+        cv2.putText(frame, str(label_id), (left, top-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+class SegmentDetect(QObject):
+    
+    image = Signal(np.ndarray)
+    result = Signal(np.ndarray)
+
+    def __init__(self, model):
+        QObject.__init__(self)
+        self.seg_detector = Segmentor('model/'+model, 'cpu', 0)
+    
+    def detect(self, frame):
+        frame_org = frame
+        seg = self.seg_detector(frame)
+
+        print(seg.shape)
+
+        if seg.dtype == np.float32:
+            seg = np.argmax(seg, axis=0)
+
+        palette = self.get_palette()
+
+        color_seg = np.zeros((seg.shape[0], seg.shape[1], 3), dtype=np.uint8)
+        for label, color in enumerate(palette):
+            color_seg[seg == label, :] = color
+        # convert to BGR
+        color_seg = color_seg[..., ::-1]
+
+        frame = frame * 0.5 + color_seg * 0.5
+        frame = frame.astype(np.uint8)
+
+        self.image.emit(frame_org)
+        self.result.emit(frame)
+    
+    def get_palette(self, num_classes=256):
+        state = np.random.get_state()
+        # random color
+        np.random.seed(42)
+        palette = np.random.randint(0, 256, size=(num_classes, 3))
+        np.random.set_state(state)
+        return [tuple(c) for c in palette]
+    
+
+class PoseDetect(QObject):
+    
+    image = Signal(np.ndarray)
+    result = Signal(np.ndarray)
+    finished = Signal()
+
+    def __init__(self, model):
+        QObject.__init__(self)
+        self.detector = Detector('model/yolov8', 'cpu', 0)
+        self.pose_detector = PoseDetector('model/pose', 'cpu', 0)
+
+    def detect(self, frame):
+        frame_org = frame.copy()
+     
+        # apply detector
+        bboxes, labels, _ = self.detector(frame)
+        keep = np.logical_and(labels == 0, bboxes[..., 4] > 0.6)
+        bboxes = bboxes[keep, :4]
+        result = self.pose_detector(frame, bboxes)
+        # draw result
+        frame = self.visualize(frame, result, 0.5, 1280)
+            
+        self.image.emit(frame_org)
+        self.result.emit(frame)
+
+    def visualize(self, frame, keypoints, thr=0.5, resize=1280):
+        skeleton = [(15, 13), (13, 11), (16, 14), (14, 12), (11, 12), (5, 11),
+                    (6, 12), (5, 6), (5, 7), (6, 8), (7, 9), (8, 10), (1, 2),
+                    (0, 1), (0, 2), (1, 3), (2, 4), (3, 5), (4, 6)]
+        palette = [(255, 128, 0), (255, 153, 51), (255, 178, 102), (230, 230, 0),
+                (255, 153, 255), (153, 204, 255), (255, 102, 255),
+                (255, 51, 255), (102, 178, 255),
+                (51, 153, 255), (255, 153, 153), (255, 102, 102), (255, 51, 51),
+                (153, 255, 153), (102, 255, 102), (51, 255, 51), (0, 255, 0),
+                (0, 0, 255), (255, 0, 0), (255, 255, 255)]
+        link_color = [
+            0, 0, 0, 0, 7, 7, 7, 9, 9, 9, 9, 9, 16, 16, 16, 16, 16, 16, 16
+        ]
+        point_color = [16, 16, 16, 16, 16, 9, 9, 9, 9, 9, 9, 0, 0, 0, 0, 0, 0]
+
+        scale = resize / max(frame.shape[0], frame.shape[1])
+
+        scores = keypoints[..., 2]
+        keypoints = (keypoints[..., :2] * scale).astype(int)
+
+        frame = cv2.resize(frame, (0, 0), fx=scale, fy=scale)
+        for kpts, score in zip(keypoints, scores):
+            show = [0] * len(kpts)
+            for (u, v), color in zip(skeleton, link_color):
+                if score[u] > thr and score[v] > thr:
+                    cv2.line(frame, kpts[u], tuple(kpts[v]), palette[color], 1,
+                            cv2.LINE_AA)
+                    show[u] = show[v] = 1
+            for kpt, show, color in zip(kpts, show, point_color):
+                if show:
+                    cv2.circle(frame, kpt, 1, palette[color], 2, cv2.LINE_AA)
+        return frame
+
+
+class MainWindow(QMainWindow):
+    
+    begin = Signal()
+    
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.label_1 = self.ui.label_1
+        self.label_2 = self.ui.label_2
+        self.model_selector = self.ui.model
+        # 读取model目录项的目录名
+        self.model_dir = os.listdir('model')
+        print(self.model_dir)
+        # 设置model_selector的选项
+        self.model_selector.addItems(self.model_dir)
+        self.model_selector.setCurrentIndex(0)
+        self.model_selector.currentTextChanged.connect(self.text_changed)
+
+        self.detector_thread = QThread()
+
+    def text_changed(self):
+        model = self.model_selector.currentText()
+
+        if model == 'yolov8':
+                self.detector = YoloDetect(model)
+        elif model == 'seg':
+                self.detector = SegmentDetect(model)
+        elif model == 'pose':
+                self.detector = PoseDetect(model)
+
+        if not self.detector_thread.isRunning():
+
+            self.videoPlayer = VideoPlayer('04.mp4')
+            self.videoPlayer.set_detector(self.detector)
+        
+            self.videoPlayer.detector.image.connect(lambda x: self.show_image(x, self.label_1))
+            self.videoPlayer.detector.result.connect(lambda x: self.show_image(x, self.label_2))
+
+            self.begin.connect(self.videoPlayer.playVideo)
+
+            self.videoPlayer.moveToThread(self.detector_thread)
+            self.videoPlayer.finished.connect(self.videoPlayer.deleteLater)
+            self.videoPlayer.finished.connect(self.detector_thread.deleteLater)
+            self.detector_thread.start()
+            self.begin.emit()
+        else:
+            self.videoPlayer.set_detector(self.detector)
+            self.videoPlayer.detector.image.connect(lambda x: self.show_image(x, self.label_1))
+            self.videoPlayer.detector.result.connect(lambda x: self.show_image(x, self.label_2))
+
+    def closeEvent(self, event):
+        if self.detector_thread.isRunning:
+            self.detector_thread.terminate()
+            self.detector_thread.wait()
+            self.videoPlayer.cap.release()
+        event.accept()
+
+    @staticmethod
+    def show_image(img_src, label):
+        image = QImage(img_src, img_src.shape[1], img_src.shape[0], QImage.Format_BGR888)
+        image = image.scaled(label.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation)
+        label.setPixmap(QPixmap.fromImage(image))
+
+if __name__ == "__main__":
+    app = QApplication([])
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
---
+++ main.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'main.ui'
+##
+## Created by: Qt User Interface Compiler version 6.4.3
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
+    QCursor, QFont, QFontDatabase, QGradient,
+    QIcon, QImage, QKeySequence, QLinearGradient,
+    QPainter, QPalette, QPixmap, QRadialGradient,
+    QTransform)
+from PySide6.QtWidgets import (QApplication, QComboBox, QHBoxLayout, QLabel,
+    QMainWindow, QMenu, QMenuBar, QSizePolicy,
+    QSpacerItem, QStatusBar, QVBoxLayout, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(1190, 638)
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        MainWindow.setSizePolicy(sizePolicy)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.model = QComboBox(self.centralwidget)
+        self.model.setObjectName(u"model")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.model.sizePolicy().hasHeightForWidth())
+        self.model.setSizePolicy(sizePolicy1)
+
+        self.horizontalLayout_2.addWidget(self.model)
+
+        self.comboBox = QComboBox(self.centralwidget)
+        self.comboBox.setObjectName(u"comboBox")
+        sizePolicy1.setHeightForWidth(self.comboBox.sizePolicy().hasHeightForWidth())
+        self.comboBox.setSizePolicy(sizePolicy1)
+
+        self.horizontalLayout_2.addWidget(self.comboBox)
+
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_2.addItem(self.horizontalSpacer)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.label_1 = QLabel(self.centralwidget)
+        self.label_1.setObjectName(u"label_1")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.label_1.sizePolicy().hasHeightForWidth())
+        self.label_1.setSizePolicy(sizePolicy2)
+        self.label_1.setMinimumSize(QSize(0, 0))
+
+        self.horizontalLayout.addWidget(self.label_1)
+
+        self.label_2 = QLabel(self.centralwidget)
+        self.label_2.setObjectName(u"label_2")
+        sizePolicy2.setHeightForWidth(self.label_2.sizePolicy().hasHeightForWidth())
+        self.label_2.setSizePolicy(sizePolicy2)
+        self.label_2.setMinimumSize(QSize(0, 0))
+        self.label_2.setTextFormat(Qt.AutoText)
+
+        self.horizontalLayout.addWidget(self.label_2)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        self.verticalLayout.setStretch(0, 1)
+        self.verticalLayout.setStretch(1, 16)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 1190, 21))
+        self.menu = QMenu(self.menubar)
+        self.menu.setObjectName(u"menu")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.menubar.addAction(self.menu.menuAction())
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.label_1.setText("")
+        self.label_2.setText("")
+        self.menu.setTitle(QCoreApplication.translate("MainWindow", u"\u6587\u4ef6", None))
+    # retranslateUi
+
