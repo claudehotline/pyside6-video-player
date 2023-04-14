49c11dfbc3eab3b1ec3eab14c78cbb52fd7afe79
Administrator <admin@example.com>
2023-04-14 18:40:03 +0800

    add pose estimation visulization functions

--- analyzer/PoseDetector.py
+++ analyzer/PoseDetector.py
@@ -14,13 +14,51 @@ class PoseDetect():
         # apply detector
         bboxes, labels, _ = self.detector(frame)
         # print(bboxes)
-        keep = np.logical_and(labels == 0, bboxes[..., 4] > 0.6)
+        keep = np.logical_and(labels == 0, bboxes[..., 4] > 0.3)
         bboxes = bboxes[keep, :4]
+        print("bboxes = ", bboxes)
         result = self.pose_detector(frame, bboxes)
+
+        print(result)
         # draw result
-        frame = self.visualize(frame, result, 0.1, 1280)
+        frame = self.visualize_hand(frame, result, 0.1, 1280)
 
         return frame  
+    
+    def visualize_hand(self, frame, keypoints, thr=0.5, resize=1280):
+        skeleton = [(0, 1), (1, 2), (2, 3), (3, 4), (0, 5), (5, 6), (6, 7),
+                    (7, 8), (0, 9), (9, 10), (10, 11), (11, 12), (0, 13),
+                    (13, 14), (14, 15), (15, 16), (0, 17), (17, 18), (18, 19),
+                    (19, 20)]
+        palette = [(255, 128, 0), (255, 153, 51), (255, 178, 102), (230, 230, 0),
+                (255, 153, 255), (153, 204, 255), (255, 102, 255),
+                (255, 51, 255), (102, 178, 255),
+                (51, 153, 255), (255, 153, 153), (255, 102, 102), (255, 51, 51),
+                (153, 255, 153), (102, 255, 102), (51, 255, 51), (0, 255, 0),
+                (0, 0, 255), (255, 0, 0), (255, 255, 255)]
+        link_color = [
+            0, 0, 0, 0, 7, 7, 7, 7, 9, 9, 9, 9, 16, 16, 16, 16, 16, 16, 16
+        ]
+        point_color = [16, 16, 16, 16, 16, 9, 9, 9, 9, 9, 9, 0, 0, 0, 0, 0, 0]
+
+        scale = resize / max(frame.shape[0], frame.shape[1])
+
+        scores = keypoints[..., 2]
+        keypoints = (keypoints[..., :2] * scale).astype(int)
+
+        frame = cv2.resize(frame, (0, 0), fx=scale, fy=scale)
+        for kpts, score in zip(keypoints, scores):
+            show = [0] * len(kpts)
+            for (u, v), color in zip(skeleton, link_color):
+                if score[u] > thr and score[v] > thr:
+                    cv2.line(frame, kpts[u], tuple(kpts[v]), palette[color], 1,
+                            cv2.LINE_AA)
+                    show[u] = show[v] = 1
+            for kpt, show, color in zip(kpts, show, point_color):
+                if show:
+                    cv2.circle(frame, kpt, 1, palette[color], 2, cv2.LINE_AA)
+        return frame
+
 
     def visualize(self, frame, keypoints, thr=0.5, resize=1280):
         skeleton = [(15, 13), (13, 11), (16, 14), (14, 12), (11, 12), (5, 11),
