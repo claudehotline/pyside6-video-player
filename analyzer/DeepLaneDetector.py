e84d67862dc25144db03e3b50b4810ba419aac24
Administrator <admin@example.com>
2023-04-23 18:08:24 +0800

    minor modification

--- analyzer/DeepLaneDetector.py
+++ analyzer/DeepLaneDetector.py
@@ -25,7 +25,7 @@ class DeepLaneDetector:
           transforms.Resize((288, 800)),
           transforms.ToTensor(),
           transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
-    ])
+        ])
     def detect(self, frame):
         vis = frame.copy()
         img_w, img_h = frame.shape[1], frame.shape[0]
